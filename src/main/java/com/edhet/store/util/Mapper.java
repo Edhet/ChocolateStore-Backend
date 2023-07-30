41e43aa873e27436b7883593f1547347b2fec69d
Eduardo P <edtvports@gmail.com>
2023-07-30 16:20:08 -0300

    Corrected password not encrypting bug

--- src/main/java/com/edhet/store/util/Mapper.java
+++ src/main/java/com/edhet/store/util/Mapper.java
@@ -3,17 +3,21 @@ package com.edhet.store.util;
 import com.edhet.store.security.registration.RegistrationRequest;
 import com.edhet.store.user.User;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class Mapper {
+
+    private final PasswordEncoder passwordEncoder;
+
     public User registrationRequestToUser(RegistrationRequest request) {
         return new User(
                 request.firstName(),
                 request.surname(),
                 request.email(),
-                request.password(),
+                passwordEncoder.encode(request.password()),
                 request.birthDate(),
                 request.gender()
         );
