2d1f615df6fecc037eda25f03e51a9a064700a2b
Eduardo P <edtvports@gmail.com>
2023-07-30 17:50:26 -0300

    Now doing this works somehow?

--- src/main/java/com/edhet/store/util/Mapper.java
+++ src/main/java/com/edhet/store/util/Mapper.java
@@ -7,14 +7,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class Mapper {
+    private final PasswordEncoder passwordEncoder;
 
     public User registrationRequestToUser(RegistrationRequest request) {
         return new User(
                 request.firstName(),
                 request.surname(),
                 request.email(),
-                request.password(),
+                passwordEncoder.encode(request.password()),
                 request.birthDate(),
                 request.gender()
         );
