76806b11c57378df89c8bfe17e2a25a436219066
Eduardo P <edtvports@gmail.com>
2023-08-01 15:12:43 -0300

    Get product from get request

--- src/main/java/com/edhet/store/product/ProductController.java
+++ src/main/java/com/edhet/store/product/ProductController.java
@@ -2,6 +2,7 @@ package com.edhet.store.product;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +19,8 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
+    @GetMapping("{id}")
+    public Product getProduct(@PathVariable String id) {
+        return productService.getProductFromRequest(id);
+    }
 }
--- src/main/java/com/edhet/store/product/ProductService.java
+++ src/main/java/com/edhet/store/product/ProductService.java
@@ -1,6 +1,7 @@
 package com.edhet.store.product;
 
 import com.edhet.store.category.CategoryService;
+import com.edhet.store.exception.errors.BadRequestException;
 import com.edhet.store.exception.errors.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,5 +29,15 @@ public class ProductService {
         return productRepository
                 .findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("No product with id: " + id));
+    }
+
+    public Product getProductFromRequest(String id) throws BadRequestException {
+        long expectedId;
+        try {
+            expectedId = Long.parseLong(id);
+        } catch (NumberFormatException e) {
+            throw new BadRequestException(e.getMessage());
+        }
+        return getProduct(expectedId);
     }
 }
